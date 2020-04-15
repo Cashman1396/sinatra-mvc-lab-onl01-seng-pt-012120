a349122a7b0fae8695c3219ae9ddb44c5911b143
Chris Ashman <chrisashman96@gmail.com>
2020-04-15 17:48:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinizer(params[:user_phrase])
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
 
